b1e1090b5ac423d966986fe473b67aa4b698e30c
honghdu <160749538+honghdu@users.noreply.github.com>
2024-08-24 23:03:00 +0700

    Bai 3.12.cpp

--- Bai 3.12.cpp
+++ Bai 3.12.cpp
@@ -6,7 +6,7 @@ using namespace std ;
 int main ()
 {
     double x, f;
-    cout << "Nhap x = "; cin >> x;
+    cin >> x;
     if (x <= -2)    f = cbrt(x);
     else    if (x >= 1)    f = log(x);
             else    f = fabs(12 * x - sin(x)) + (28 * cos(x));
