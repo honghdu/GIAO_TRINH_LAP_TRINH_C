f2c9eb6bb9d8706ab33068688f995ae0f8ef0438
honghdu <160749538+honghdu@users.noreply.github.com>
2024-08-24 22:56:31 +0700

    Bai 3.9.cpp

--- Bai 3.9.cpp
+++ Bai 3.9.cpp
@@ -1,13 +1,13 @@
-//Bài 3.9
+//BÃ i 3.9
 #include<iostream>
 
 using namespace std ;
 
 int main ()
 {
-    long long d;
-    cin >> d;
-    d *= 10;
-    cout << 4 * d / 5;
+    long long a;
+    cin >> a;
+    a *= 10;
+    cout << 4 * a / 5;
     return 0;
 }
