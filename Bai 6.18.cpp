7f9615f1d861bb50828949e7420bb169fc16a367
honghdu <160749538+honghdu@users.noreply.github.com>
2024-08-25 14:15:58 +0700

    Bai 6.18.cpp

--- Bai 6.18.cpp
+++ Bai 6.18.cpp
@@ -13,10 +13,9 @@ int main ()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    cin >> n;
+    cin >> n >> x;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
-    cin >> x;
     for(int i = 1; i <= n; i++)
         if(a[i] == x)
         {
@@ -54,10 +53,9 @@ int main ()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    cin >> n;
+    cin >> n >> x;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
-    cin >> x;
     for(int i = 1; i <= n; i++)
         if(a[i] == x)
         {
