7eb2efbf039a546ace66270713651845f5f859e5
honghdu <160749538+honghdu@users.noreply.github.com>
2024-09-04 22:12:57 +0700

    Update Bai 9.5.cpp

--- Bai 9.5.cpp
+++ Bai 9.5.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    int a[MAX][MAX], b[MAX][MAX], c[MAX][MAX], m, n, p;
+    int a[MAX][MAX], b[MAX][MAX], c[MAX][MAX], m, n, p, k;
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     if(fopen("BAI5.INP", "r"))
@@ -14,26 +14,28 @@ int main()
         freopen("BAI5.INP", "r", stdin);
         freopen("BAI5.OUT", "w", stdout);
     }
-    cin >> m >> n >> p;
+
+    cin >> m >> n;
     for(int i = 1; i <= m; i++)
         for(int j = 1; j <= n; j++)
             cin >> a[i][j];
 
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= p; j++)
+    cin >> p >> k;
+    for(int i = 1; i <= p; i++)
+        for(int j = 1; j <= k; j++)
             cin >> b[i][j];
-
+    if(n == p)
     for(int i = 1; i <= m; i++)
-        for(int j = 1; j <= p; j++)
+        for(int j = 1; j <= k; j++)
         {
             c[i][j] = 0;
-            for(int k = 1; k <= n; k++)
-                c[i][j] += a[i][k] * b[k][j];
+            for(int h = 1; h <= n; h++)
+                c[i][j] += a[i][h] * b[h][j];
         }
 
     for(int i = 1; i <= m; i++)
     {
-        for(int j = 1; j <= p; j++)
+        for(int j = 1; j <= k; j++)
             cout << c[i][j] << " ";
         cout << '\n';
     }
